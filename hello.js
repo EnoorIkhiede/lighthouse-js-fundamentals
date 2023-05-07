98ab5eaff32707cef96b97eccdff23ef39cb4f87
Enoor <enoor.ikhiede@gmail.com>
2023-05-07 14:15:41 -0600

    Say Hello Declare and calling functions

---
+++ hello.js
@@ -0,0 +1,27 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
+
+
